f9514f8804b327d33ade25c7cf6847a421e20668
marysitz <marysitz@gmail.com>
2015-12-26 17:04:14 -0500

    eloquent js exercises

---
+++ eloquent-javascript/02-functions/01-minimum.js
@@ -0,0 +1,8 @@
+// Write a function min that takes two arguments and returns their minimum.
+
+function min(x, y) {
+	var z = Math.min(x, y);
+	return z;
+}
+console.log(min(0, 10));
+console.log(min(0, -10));
